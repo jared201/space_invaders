708e4e9477912d7dc2e9a549a97038631eb260df
Jared Odulio <jared.odulio@gmail.com>
2024-05-03 09:13:42 +0800

    fix enemy movement

--- src/components/sprites/BasicEnemy.vue
+++ src/components/sprites/BasicEnemy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="basic-enemy">
+  <div class="basic-enemy" v-bind:style="{ left: x + 'px', top: y + 'px' }">
     <svg class="blinking-enemy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="pink" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M12 2L2 7l10 5 10-5-10-5z"></path>
       <path d="M2 7v7l10 5 10-5V7"></path>
@@ -10,7 +10,49 @@
     </svg>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      x: 0, // initial x position
+      y: 0, // initial y position
+      direction: 'right', // initial direction
+    };
+  },
+  methods: {
+    move() {
+      const canvasWidth = this.$parent.width; // assuming the parent component has a width property
 
+      // Change direction and move down if the enemy reaches the edge of the canvas
+      if (this.x <= 0) {
+        this.direction = 'right';
+      } else if (this.x >= canvasWidth - 50) { // subtract the width of the enemy to prevent it from going off the canvas
+        this.direction = 'left';
+      }
+
+      // Move left or right depending on the direction
+      if (this.direction === 'right') {
+        this.x += 10; // adjust this value to change the speed of horizontal movement
+      } else {
+        this.x -= 10; // adjust this value to change the speed of horizontal movement
+      }
+
+      // Move down every second
+      if (Date.now() % 100 === 0) {
+        this.y += 10; // adjust this value to change the speed of downward movement
+      }
+    },
+  },
+  mounted() {
+    // Call the move method every 100ms
+    this.interval = setInterval(this.move, 100);
+  },
+  beforeDestroy() {
+    // Clear the interval when the component is destroyed
+    clearInterval(this.interval);
+  },
+};
+</script>
 <style scoped>
 .basic-enemy {
   position: absolute;
